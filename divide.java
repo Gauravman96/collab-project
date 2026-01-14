39413c8f42e616a2f88c595f042dfe0a45778b1e
Jyoti Upadhyay <jyoti.upadhyay_cs22@gla.ac.in>
2026-01-14 11:22:52 +0530

    added files

---
+++ divide.java
@@ -0,0 +1,10 @@
+class divide{
+    public static void main(String[] args) {
+        int num1=4;
+        int num2=16;
+        int res=num2/num1;
+
+        System.out.println("Divded");
+        
+    }
+}
